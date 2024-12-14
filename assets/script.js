b607d5fd5ad739f31946846ce2de3bbb5e01513c
Christopher Hahn <chahn970@gmail.com>
2024-12-14 14:34:22 -0700

    complete reset button logic

--- assets/script.js
+++ assets/script.js
@@ -21,28 +21,42 @@ function Players(){
 }
 
 function Game(){
-    let players = Players();
+    let gameOver = false;
+    const players = Players();
     const board = [];
-    for (let i = 0; i < 3; i++) {
-        board[i] = [];
-        for (let j = 0; j < 3; j++) {
-            board[i].push(null);
+
+    const boardSetup = () => {
+        for (let i = 0; i < 3; i++) {
+            board[i] = [];
+            for (let j = 0; j < 3; j++) {
+                board[i].push(null);
+            }
         }
     }
     const getBoard = () => board;
 
+    const resetGameStatus = () => {
+        if(gameOver === true){
+            gameOver = false;
+        }
+    };
+
     const placeMark = (row, col, player = players.getActivePlayer()) => {
-        if(row < 3 && col < 3){
-            if(board[row][col] == null){
-                board[row][col] = player.token;
-                console.table(board);
-                checkWin(board);
-                players.switchPlayer();
+        if(gameOver == false){
+            if(row < 3 && col < 3){
+                if(board[row][col] == null){
+                    board[row][col] = player.token;
+                    console.table(board);
+                    checkWin(board);
+                    players.switchPlayer();
+                }else{
+                    alert('place is already taken');
+                }
             }else{
-                alert('place is already taken');
+                alert('invalid number, please try again');
             }
         }else{
-            alert('invalid number, please try again');
+            alert('the game is over, start a new game');
         }
     }
 
@@ -50,26 +64,40 @@ function Game(){
         for(let i = 0; i < 3; i++){
             if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]){
                 console.log(`${player.name} wins`);
+                announceWinner(player.name);
                 return;
             }else if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]){
                 console.log(`${player.name} wins`);
+                announceWinner(player.name);
                 return;
             }else if (board[1][1] && board[1][1] === board[0][0] && board[0][0] === board[2][2]){
                 console.log(`${player.name} wins`);
+                announceWinner(player.name);
                 return;
             }else if (board[1][1] && board[1][1] === board[0][2] && board[0][2] === board[2][0]){
                 console.log(`${player.name} wins`);
+                announceWinner(player.name);
                 return;
             }
         }
     }
-    return {getBoard, placeMark};
+
+    const announceWinner = (name) => {
+        const message = document.querySelector('.message');
+        message.textContent = `${name} wins the game`;
+        gameOver = true;
+    }
+
+    return {getBoard, placeMark, boardSetup, resetGameStatus };
 }
 
 function GameHandler(){
-    let game = Game();
-    let gameBoard = game.getBoard();
-    let gameTable = document.querySelector('.game-table');
+    const game = Game();
+    const players = Players();
+    const gameBoard = game.getBoard();
+    const gameTable = document.querySelector('.game-table');
+    const resetBtn = document.querySelector('.reset');
+    game.boardSetup();
 
     for(let [rowIndex, row] of gameBoard.entries()){
         for(let [colIndex, cell] of row.entries()){
@@ -78,15 +106,22 @@ function GameHandler(){
             cellButton.textContent = cell;
             gameTable.appendChild(cellButton);
 
-            cellButton.addEventListener('click', function(){
-                let cellCoord = this.dataset.cell;
-                let cellCoordArr = cellCoord.split(',');
-                let x = parseInt(cellCoordArr[0]);
-                let y = parseInt(cellCoordArr[1]);
-                game.placeMark(x, y);
-                updateBoard(this);
-            })
+            cellButton.addEventListener('click', placeVisualMark);
         }
+    }
+
+    function placeVisualMark(){
+        let cellCoord = this.dataset.cell;
+        let cellCoordArr = cellCoord.split(',');
+        let x = parseInt(cellCoordArr[0]);
+        let y = parseInt(cellCoordArr[1]);
+        game.placeMark(x, y);
+        updateBoard(this);
+    }
+
+    let resetBoard = () => {
+        const cellButtons = document.querySelectorAll('button[data-cell]');
+        cellButtons.forEach(item => {item.textContent = ''});
     }
 
     const updateBoard = (item) => {
@@ -104,6 +139,16 @@ function GameHandler(){
             }
         }
     }
+
+    function resetGame (){
+        let message = document.querySelector('.message');
+        game.boardSetup();
+        resetBoard();
+        game.resetGameStatus();
+        message.textContent = '';
+    }
+
+    resetBtn.addEventListener('click', resetGame);
     
 }
 
